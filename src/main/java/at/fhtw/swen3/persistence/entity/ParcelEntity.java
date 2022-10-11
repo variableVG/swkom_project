324b44b429e85522658d8e0fb8a39d5cf85c88f6
rawan <rawan.mousa.at@gmail.com>
2022-10-11 16:47:29 +0200

    start with Validation

--- src/main/java/at/fhtw/swen3/persistence/entity/ParcelEntity.java
+++ src/main/java/at/fhtw/swen3/persistence/entity/ParcelEntity.java
@@ -7,12 +7,19 @@ import at.fhtw.swen3.services.dto.ParcelDTO;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class ParcelEntity {
 
     private Parcel parcel;
     private TrackingInformation trackingInformation;
     private NewParcelInfo newParcelInfo;
+
+    @NotNull(message = "PostalCode cannot be null")
+    @Size(min = 4, max = 4, message = "A valid PostalCode must contain (A-, 4 digits, 0000-9999) ")
+    private int PostalCode;
 
     //private ParcelDTO parcelDTO;
 
