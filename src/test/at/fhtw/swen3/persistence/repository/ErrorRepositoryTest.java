2c7a996d5f5362e3302fdb94689b7b462d148e94
Violeta <vio.garciaespin@gmail.com>
2022-11-05 22:51:06 +0100

    ErrorRepository Unit Test

---
+++ src/test/at/fhtw/swen3/persistence/repository/ErrorRepositoryTest.java
@@ -0,0 +1,36 @@
+package at.fhtw.swen3.persistence.repository;
+
+import at.fhtw.swen3.persistence.entity.ErrorEntity;
+import at.fhtw.swen3.services.ParcelService;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ErrorRepositoryTest {
+
+    @Autowired
+    private ErrorRepository repo;
+
+    ErrorEntity errorEntity;
+
+    @BeforeEach
+    void setUp() {
+        errorEntity = ErrorEntity.builder().errorMessage("This is a message").build();
+    }
+
+    @Test
+    void saveTest() {
+        ErrorEntity errorEntityTest = repo.save(errorEntity);
+        assertEquals(errorEntity.getErrorMessage(), errorEntityTest.getErrorMessage());
+    }
+
+
+
+
+
+}
