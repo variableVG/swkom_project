57402c1894f84547ffa8fc6d9a379260ea0e1c98
rawan <rawan.mousa.at@gmail.com>
2022-11-06 01:01:51 +0100

    Service

--- src/main/java/at/fhtw/swen3/services/HopService.java
+++ src/main/java/at/fhtw/swen3/services/HopService.java
@@ -1,5 +1,17 @@
 package at.fhtw.swen3.services;
+import at.fhtw.swen3.services.mapper.HopMapper;
+import at.fhtw.swen3.services.mapper.ParcelMapper;
+import at.fhtw.swen3.services.validation.MyValidator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class HopService {
+    @Autowired
+    private HopMapper hopMapper;
 
+        @Autowired
+    private MyValidator myValidator;
 }
--- src/main/java/at/fhtw/swen3/services/TransferwarehouseService.java
+++ src/main/java/at/fhtw/swen3/services/TransferwarehouseService.java
@@ -1,4 +1,18 @@
 package at.fhtw.swen3.services;
 
+import at.fhtw.swen3.services.mapper.TransferwarehouseMapper;
+import at.fhtw.swen3.services.mapper.WarehouseMapper;
+import at.fhtw.swen3.services.validation.MyValidator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class TransferwarehouseService {
+    @Autowired
+    private TransferwarehouseMapper transferwarehouseMapper;
+    @Autowired
+    private MyValidator myValidator;
+
 }
