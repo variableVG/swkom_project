2e3a776e9cb0b04130a96a7bfafe5469e9ae6bbd
Violeta <vio.garciaespin@gmail.com>
2022-11-07 10:00:06 +0100

    test

--- src/main/java/at/fhtw/swen3/services/impl/GeoCoordinateImpl.java
+++ src/main/java/at/fhtw/swen3/services/impl/GeoCoordinateImpl.java
@@ -20,8 +20,4 @@ public class GeoCoordinateImpl implements GeoCoordinateService {
 
 
 
-
-
-
-
 }
--- src/test/at/fhtw/swen3/persistence/repository/HopArrivalRepositoryTest.java
+++ src/test/at/fhtw/swen3/persistence/repository/HopArrivalRepositoryTest.java
@@ -1,5 +1,9 @@
 package at.fhtw.swen3.persistence.repository;
 
+import at.fhtw.swen3.persistence.entity.HopArrivalEntity;
+import at.fhtw.swen3.services.dto.Hop;
+import at.fhtw.swen3.services.dto.HopArrival;
+import at.fhtw.swen3.services.impl.HopArrivalImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,9 +16,11 @@ class HopArrivalRepositoryTest {
     @Autowired
     HopArrivalRepository repo;
 
+    HopArrivalEntity hopArrivalEntity;
+
     @Test
     void setUp() {
-
+        hopArrivalEntity = HopArrivalEntity.builder().build();
     }
 
     @Test
