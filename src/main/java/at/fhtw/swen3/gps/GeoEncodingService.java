1f7f32a3491c3197cb398b6098bc0f980a72254c
Violeta <vio.garciaespin@gmail.com>
2022-11-19 08:29:52 +0100

    Add gps folder + GeoEncodingService + BingEncodingProxy - not implemented yet

---
+++ src/main/java/at/fhtw/swen3/gps/GeoEncodingService.java
@@ -0,0 +1,4 @@
+package at.fhtw.swen3.gps;
+
+public interface GeoEncodingService {
+}
---
+++ src/main/java/at/fhtw/swen3/gps/impl/BingEncodingProxy.java
@@ -0,0 +1,6 @@
+package at.fhtw.swen3.gps.impl;
+
+import at.fhtw.swen3.gps.GeoEncodingService;
+
+public class BingEncodingProxy implements GeoEncodingService {
+}
