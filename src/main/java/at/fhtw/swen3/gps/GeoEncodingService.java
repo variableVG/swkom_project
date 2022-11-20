90374cbdde76f9368af4b101d793d3ec7a169eff
Violeta <vio.garciaespin@gmail.com>
2022-11-20 09:53:15 +0100

    Set GeoCoordinates - oldfashion

--- src/main/java/at/fhtw/swen3/gps/GeoEncodingService.java
+++ src/main/java/at/fhtw/swen3/gps/GeoEncodingService.java
@@ -1,12 +1,11 @@
 package at.fhtw.swen3.gps;
 
-import at.fhtw.swen3.persistence.entities.GeoCoordinateEntity;
 import at.fhtw.swen3.persistence.entities.RecipientEntity;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.util.concurrent.CompletableFuture;
 
 public interface GeoEncodingService {
 
-    GeoCoordinateEntity encodeAddress(RecipientEntity r) throws IOException;
+    CompletableFuture<Object> encodeAddress(RecipientEntity r) throws IOException;
 }
--- src/main/java/at/fhtw/swen3/gps/impl/BingEncodingProxy.java
+++ src/main/java/at/fhtw/swen3/gps/impl/BingEncodingProxy.java
@@ -3,33 +3,30 @@ package at.fhtw.swen3.gps.impl;
 import at.fhtw.swen3.gps.GeoEncodingService;
 import at.fhtw.swen3.persistence.entities.GeoCoordinateEntity;
 import at.fhtw.swen3.persistence.entities.RecipientEntity;
-import io.swagger.models.Response;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.DeserializationProblemHandler;
 import lombok.extern.slf4j.Slf4j;
-import netscape.javascript.JSObject;
-import org.apache.tomcat.jni.Address;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.print.DocFlavor;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
 
 
+@Slf4j
 public class BingEncodingProxy implements GeoEncodingService {
 
     @Override
-    public GeoCoordinateEntity encodeAddress(RecipientEntity r) throws IOException {
+    public CompletableFuture<Object> encodeAddress(RecipientEntity r)  {
         UriComponents uri = UriComponentsBuilder.newInstance()
                 .scheme("https")
                 .host("nominatim.openstreetmap.org")
@@ -44,13 +41,42 @@ public class BingEncodingProxy implements GeoEncodingService {
 
         System.out.println(uri);
 
-        RestTemplate restTemplate = new RestTemplate();
-        JSONObject j = restTemplate.getForObject(uri.toString(), JSONObject.class);
-        System.out.println(j.length());
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest;
+
+        try {
+            log.debug("Class BingEncodingProxy, encodeAddress() - Send request for coordinates.");
+            System.out.println("Inside first try");
+            httpRequest = HttpRequest.newBuilder().uri(new URI(uri.toString())).build();
+        }
+        catch(Exception e) {
+            log.warn("Class BingEncodingProxy, encodeAddress() - " + e);
+            return null;
+        }
+
+
+        HttpResponse response;
+        try {
+            System.out.println("Second try2");
+            response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Response is");
+        System.out.println(response);
+        System.out.println("Directions response is ");
+        System.out.println(response.body());
+
+
+        //RestTemplate restTemplate = new RestTemplate();
+        //JSONObject j = restTemplate.getForObject(uri.toString(), JSONObject.class);
+        //System.out.println(j.length());
 
         //Source:
         // https://www.baeldung.com/java-http-response-body-as-string
 
         return null;
     }
+
+
 }
