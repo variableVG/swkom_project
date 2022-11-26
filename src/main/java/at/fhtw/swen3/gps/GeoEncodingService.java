5ca91a5440dbb69c8132c55760eb2ee4f64bd8dc
Violeta <vio.garciaespin@gmail.com>
2022-11-26 14:43:49 +0100

    Refractor gps folder, add service folder Complete implementation of BingEncodingProxy is still missing

--- src/main/java/at/fhtw/swen3/gps/GeoEncodingService.java
+++
@@ -1,7 +0,0 @@
-package at.fhtw.swen3.gps;
-
-import at.fhtw.swen3.services.GeoCoordinateService;
-
-public interface GeoEncodingService {
-
-}
---
+++ src/main/java/at/fhtw/swen3/gps/service/impl/BingEncodingProxy.java
@@ -0,0 +1,65 @@
+package at.fhtw.swen3.gps.service.impl;
+
+import at.fhtw.swen3.gps.service.GeoEncodingService;
+import at.fhtw.swen3.persistence.entities.RecipientEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class BingEncodingProxy implements GeoEncodingService {
+    @Override
+    public CompletableFuture<Object> encodeAddress(RecipientEntity r)  {
+        UriComponents uri = UriComponentsBuilder.newInstance()
+                .scheme("https")
+                .host("nominatim.openstreetmap.org")
+                .path("/search")
+                .queryParam("street", r.getStreet())
+                .queryParam("city", r.getCity())
+                .queryParam("postalcode", r.getPostalCode())
+                .queryParam("country", r.getCountry())
+                .queryParam("format", "geojson")
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest;
+
+        try {
+            log.debug("Class BingEncodingProxy, encodeAddress() - Send request for coordinates.");
+            System.out.println("Inside first try");
+            httpRequest = HttpRequest.newBuilder().uri(new URI(uri.toString())).build();
+        }
+        catch(Exception e) {
+            log.warn("Class BingEncodingProxy, encodeAddress() - " + e);
+            return null;
+        }
+
+
+        HttpResponse response;
+        try {
+            System.out.println("Second try2");
+            response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Response is");
+        System.out.println(response);
+        System.out.println("Directions response is ");
+        System.out.println(response.body());
+
+        //TODO Parse response and assig to coordinates
+
+
+        return null;
+    }
+
+}
+
+
