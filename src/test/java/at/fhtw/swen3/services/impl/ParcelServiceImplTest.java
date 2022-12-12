43e9b3ff584094cb4c030d11daebec1938e9608d
Violeta <vio.garciaespin@gmail.com>
2022-12-12 09:47:01 +0100

    Refractor ParcelImplTest to ParcelServiceImplTest

--- src/test/java/at/fhtw/swen3/services/impl/ParcelServiceImplTest.java
+++ src/test/java/at/fhtw/swen3/services/impl/ParcelServiceImplTest.java
@@ -13,7 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 @Slf4j
 @SpringBootTest
-class ParcelImplTest {
+class ParcelServiceImplTest {
 
     @Autowired
     private ParcelServiceImpl parcelImpl;
