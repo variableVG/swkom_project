ea14ae302a8b768d95916e8ae83c001516381178
rawan <rawan.mousa.at@gmail.com>
2022-12-31 00:23:21 +0100

    Integration test Packages

---
+++ src/integration-test/java/at/fhtw/swen3/ParcelIntegrationTest.java
@@ -0,0 +1,10 @@
+package at.fhtw.swen3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class ParcelIntegrationTest {
+
+}
