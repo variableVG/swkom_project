e1b1c9d28c8eb9a5b2c65a92e98bac8655f7d2c9
RawanMousa9896 <rawan.mousa.at@gmail.com>
2023-01-08 19:57:26 +0100

    WarehouseValidationTest

--- src/main/java/at/fhtw/swen3/persistence/DALException.java
+++ src/main/java/at/fhtw/swen3/persistence/DALException.java
@@ -8,6 +8,30 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import at.fhtw.swen3.persistence.entities.ErrorEntity;
+import lombok.Getter;
+
+@Getter
+public class DALException extends Exception {
+
+    private Exception innerException;
+
+    private ErrorEntity errorEntity;
+
+
+    public DALException(long errorId, String errorMsg, Exception e) {
+        this.innerException = e;
+        this.errorEntity = ErrorEntity.builder().errorMessage(errorMsg).id(errorId).build();
+
+    }
+
+    @Override
+    public String getMessage() {
+        return errorEntity.getErrorMessage();
+    }
+
+}
+/*
 @ControllerAdvice
 public class DALException extends ResponseEntityExceptionHandler {
 
@@ -22,3 +46,6 @@ public class DALException extends ResponseEntityExceptionHandler {
                 new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 }
+
+
+ */
