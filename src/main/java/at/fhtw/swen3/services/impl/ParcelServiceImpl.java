50cb681f73abc15d2f993419669ae348288f9290
RawanMousa9896 <rawan.mousa.at@gmail.com>
2023-01-13 12:10:45 +0100

    last pipeline test

--- src/main/java/at/fhtw/swen3/services/impl/ParcelServiceImpl.java
+++ src/main/java/at/fhtw/swen3/services/impl/ParcelServiceImpl.java
@@ -42,7 +42,6 @@ public class ParcelServiceImpl implements ParcelService {
 
     @Autowired
     private MyValidator myValidator;
-
     private GeoEncodingService geoEncodingService;
 
     public HopEntity findNearestHop (Double lat, Double lon) {
